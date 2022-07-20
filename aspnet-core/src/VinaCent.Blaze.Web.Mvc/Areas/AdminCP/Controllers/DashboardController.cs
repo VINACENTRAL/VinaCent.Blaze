3b4ba11cb945f4cdbb8e8f92272c7895dd2645bc
Minh <baominh9821@gmail.com>
2022-07-20 11:41:05 +0700

    add theme, add Dashboard page

---
+++ aspnet-core/src/VinaCent.Blaze.Web.Mvc/Areas/AdminCP/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VinaCent.Blaze.Web.Areas.AdminCP.Controllers;
+
+public class Dashboard : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ aspnet-core/src/VinaCent.Blaze.Web.Mvc/Views/Shared/Themes/Velzon/Components/Vertical/HeaderUserArea/HeaderUserAreaViewComponent.cs
@@ -0,0 +1,6 @@
+﻿namespace VinaCent.Blaze.Web.Views.Shared.Themes.Velzon.Components.Vertical.HeaderUserArea;
+
+public class HeaderUserAreaViewComponent
+{
+    
+}
