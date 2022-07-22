5ca945caa320e03b802ce1fbb28ea2812124f5de
Minh <baominh9821@gmail.com>
2022-07-22 16:26:05 +0700

    Create file management service

--- aspnet-core/src/VinaCent.Blaze.Application/AppCore/FileUnits/IFileUnitAppService.cs
+++ aspnet-core/src/VinaCent.Blaze.Application/AppCore/FileUnits/IFileUnitAppService.cs
@@ -1,6 +1,7 @@
 ﻿using Abp.Application.Services;
 using Abp.Application.Services.Dto;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using VinaCent.Blaze.AppCore.FileUnits.Dto;
 
@@ -8,12 +9,19 @@ namespace VinaCent.Blaze.AppCore.FileUnits
 {
     public interface IFileUnitAppService : IApplicationService
     {
+        public string GetAppContentPath { get; }
+        public string GetCurrentTenantContentPath { get; }
+        public string GetCurrentUserContentPath { get; }
+        public string GetTodayContentPath { get; }
         Task<FileUnitDto> GetAsync(Guid id);
         Task<PagedResultDto<FileUnitDto>> GetAllAsync(PagedFileUnitResultRequestDto input);
+        Task<List<FileUnitDto>> GetAllParentAsync(string directory);
+        Task<FileUnitDto> GetByFullName(string fullName);
+        Task<FileUnitDto> GetParentAsync(string directory);
         Task<FileUnitDto> UploadFileAsync(UploadFileUnitDto input);
         Task<FileUnitDto> CreateDirectoryAsync(CreateDirectoryDto input);
-        Task<FileUnitDto> RenameAsync(UpdateFileUnitDto input);
-        Task<FileUnitDto> MoveAsync(Guid id, string directory);
+        Task<FileUnitDto> RenameAsync(FileUnitRenameDto input);
+        Task<FileUnitDto> MoveAsync(Guid id, Guid directoryId);
         Task DeleteAsync(Guid id);
     }
 }
--- aspnet-core/src/VinaCent.Blaze.Web.Mvc/Views/Shared/Components/VelzonAside/SideBarMenuViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Abp.Application.Navigation;
-
-namespace VinaCent.Blaze.Web.Views.Shared.Components.VelzonAside
-{
-    public class SideBarMenuViewModel
-    {
-        public UserMenu MainMenu { get; set; }
-    }
-}
