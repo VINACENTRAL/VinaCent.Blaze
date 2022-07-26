7c0613d110d8a8b0912903f64ec5ddf24c0c29d7
Rogzi <rogzi.tiger@gmail.com>
2022-07-26 09:48:03 +0700

    Done #25

--- aspnet-core/src/VinaCent.Blaze.Core/Configuration/AppSettingNames.cs
+++ aspnet-core/src/VinaCent.Blaze.Core/Configuration/AppSettingNames.cs
@@ -2,12 +2,22 @@
 {
     public static class AppSettingNames
     {
+        #region App meta
+        public const string SiteTitle = "App.SiteTitle";
         public const string SiteName = "App.SiteName";
+        public const string SiteDescription = "App.SiteDescription";
+        public const string SiteAuthor = "App.Author";
+        public const string SiteAuthorProfileUrl = "App.AuthorProfileUrl";
+        #endregion
 
+        #region App UI/Theme
         public const string UiTheme = "App.UiTheme";
+        #endregion
 
+        #region App File Management
         public const string AllowedMaxFileSize = "App.AllowedMaxFileSize";
 
         public const string AllowedUploadFormats = "App.AllowedUploadFormats";
+        #endregion
     }
 }
