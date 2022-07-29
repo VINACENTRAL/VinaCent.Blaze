a54ec5a7b9d09bd117e319167d7e6a1598af2178
Metsubojinrai.net <64258995+Metsubojinrai@users.noreply.github.com>
2022-07-29 08:46:53 +0700

    Update auditlog UI (#60)
    update audit logs Ui
    Co-authored-by: Minh <baominh9821@gmail.com>

--- aspnet-core/src/VinaCent.Blaze.Application/AppCore/AuditLogs/Dto/AuditLogListDto.cs
+++ aspnet-core/src/VinaCent.Blaze.Application/AppCore/AuditLogs/Dto/AuditLogListDto.cs
@@ -2,6 +2,7 @@
 using Abp.Auditing;
 using Abp.AutoMapper;
 using Abp.Domain.Entities;
+using Abp.Localization;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -17,6 +18,7 @@ namespace VinaCent.Blaze.AppCore.AuditLogs.Dto
         /// <summary>
         /// TenantId.
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.TenantId)]
         public int? TenantId { get; set; }
 
         /// <summary>
@@ -27,21 +29,25 @@ namespace VinaCent.Blaze.AppCore.AuditLogs.Dto
         /// <summary>
         /// 
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.UserName)]
         public string UserName { get; set; }
 
         /// <summary>
         /// Service (class/interface) name.
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.ServiceName)]
         public string ServiceName { get; set; }
 
         /// <summary>
         /// Executed method name.
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.MethodName)]
         public string MethodName { get; set; }
 
         /// <summary>
         /// Start time of the method execution.
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.ExecutionTime)]
         public DateTime ExecutionTime { get; set; }
 
         public virtual string ExecutionTimeStr
@@ -57,31 +63,37 @@ namespace VinaCent.Blaze.AppCore.AuditLogs.Dto
         /// <summary>
         /// Total duration of the method call as milliseconds.
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.ExecutionDuration)]
         public int ExecutionDuration { get; set; }
 
         /// <summary>
         /// IP address of the client.
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.ClientIpAddress)]
         public string ClientIpAddress { get; set; }
 
         /// <summary>
         /// Name (generally computer name) of the client.
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.ClientName)]
         public string ClientName { get; set; }
 
         /// <summary>
         /// Browser information if this method is called in a web request.
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.BrowserInfo)]
         public string BrowserInfo { get; set; }
 
         /// <summary>
         /// Store the message content of  <see cref="Exception"/>.
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.ExceptionMessage)]
         public string ExceptionMessage { get; set; }
 
         /// <summary>
         /// 
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.HasException)]
         public bool HasException { get; set; }
 
         /// <summary>
@@ -92,11 +104,13 @@ namespace VinaCent.Blaze.AppCore.AuditLogs.Dto
         /// <summary>
         /// 
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.ImpersonatorUserName)]
         public string ImpersonatorUserName { get; set; }
 
         /// <summary>
         /// <see cref="AuditInfo.ImpersonatorTenantId"/>.
         /// </summary>
+        [AbpDisplayName(BlazeConsts.LocalizationSourceName, LKConstants.ImpersonatorTenant)]
         public int? ImpersonatorTenantId { get; set; }
     }
 }
