3beb75c46c630bd2757f917bab3824ab161adf8c
Rogzi <rogzi.tiger@gmail.com>
2022-08-01 10:41:42 +0700

    Add new Entity "TextTemplates"

--- aspnet-core/src/VinaCent.Blaze.EntityFrameworkCore/EntityFrameworkCore/BlazeDbContext.cs
+++ aspnet-core/src/VinaCent.Blaze.EntityFrameworkCore/EntityFrameworkCore/BlazeDbContext.cs
@@ -4,13 +4,19 @@ using VinaCent.Blaze.Authorization.Roles;
 using VinaCent.Blaze.Authorization.Users;
 using VinaCent.Blaze.MultiTenancy;
 using VinaCent.Blaze.AppCore.FileUnits;
+using VinaCent.Blaze.AppCore.TextTemplates;
 
 namespace VinaCent.Blaze.EntityFrameworkCore
 {
+    // dotnet ef migrations add "Add.TextTemplates" -p ./src/VinaCent.Blaze.EntityFrameworkCore -s ./src/VinaCent.Blaze.Web.Mvc --context BlazeDbContext
+    // dotnet ef database update -p ./src/VinaCent.Blaze.EntityFrameworkCore -s ./src/VinaCent.Blaze.Web.Mvc --context BlazeDbContext
+    // dotnet ef migrations remove -p ./src/VinaCent.Blaze.EntityFrameworkCore -s ./src/VinaCent.Blaze.Web.Mvc --context BlazeDbContext
     public class BlazeDbContext : AbpZeroDbContext<Tenant, Role, User, BlazeDbContext>
     {
         /* Define a DbSet for each entity of the application */
         public DbSet<FileUnit> FileUnits { get; set; }
+
+        public DbSet<TextTemplate> TextTemplates { get; set; }
 
         public BlazeDbContext(DbContextOptions<BlazeDbContext> options)
             : base(options)
---
+++ aspnet-core/src/VinaCent.Blaze.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/DefaultTextTemplatesCreator.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using VinaCent.Blaze.AppCore.TextTemplates;
+
+namespace VinaCent.Blaze.EntityFrameworkCore.Seed.Host
+{
+    public class DefaultTextTemplatesCreator
+    {
+        private readonly BlazeDbContext _context;
+
+        public DefaultTextTemplatesCreator(BlazeDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create()
+        {
+            foreach (var tt in TextTemplate.Defaults)
+            {
+                if (!_context.TextTemplates.Any(x => x.Id == tt.Id))
+                {
+                    _context.TextTemplates.Add(tt);
+                    _context.SaveChanges();
+                }
+            }
+        }
+    }
+}
