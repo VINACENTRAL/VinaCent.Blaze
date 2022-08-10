021be0ad50434b2804987b23899d681fc21b81f6
rogzi.tiger@gmail.com <Rogzi>
2022-08-10 19:35:16 +0700

    Done #125

--- aspnet-core/src/VinaCent.Blaze.Web.Mvc/wwwroot/view-resources/Views/Profiles/Index.js
+++ aspnet-core/src/VinaCent.Blaze.Web.Mvc/wwwroot/view-resources/Views/Profiles/Index.js
@@ -1,66 +1,64 @@
-﻿(function ($) {
-    $(function () {
-        var _profileService = abp.services.app.profile,
-            l = abp.localization.getSource('Blaze');
+﻿$(function () {
+    const l = abp.localization.getSource('Blaze');
 
-        $('#PersonalInfoForm').submit(function (e) {
-            e.preventDefault();
+    const _profileService = abp.services.app.profile;
+    const personalInfoForm = $('#PersonalInfoForm');
+    const changePasswordForm = $('#ChangePasswordForm');
 
-            if (!$('#PersonalInfoForm').valid()) {
-                return false;
-            }
+    personalInfoForm.submit(function (e) {
+        e.preventDefault();
 
-            var input = $('#PersonalInfoForm').serializeFormToObject();
-
-            _profileService.update(input).done(function (result) {
-                abp.notify.info(l(LKConstants.SavedSuccessfully));
-                updateConcurrencyStamp();
-            });
-        });
-
-        $('#ChangePasswordForm').submit(function (e) {
-            e.preventDefault();
-
-            if (!$('#ChangePasswordForm').valid()) {
-                return false;
-            }
-
-            var input = $('#ChangePasswordForm').serializeFormToObject();
-
-            if (
-                input.newPassword != input.newPasswordConfirm ||
-                input.newPassword == ''
-            ) {
-                abp.message.error(l(LKConstants.NewPasswordConfirmFailed));
-                return;
-            }
-
-            if (input.currentPassword && input.currentPassword == '') {
-                return;
-            }
-
-            _profileService.changePassword(input).then(function (result) {
-                abp.message.success(l(LKConstants.PasswordChanged));
-                abp.event.trigger('passwordChanged');
-                $('#ChangePasswordForm').trigger("reset");
-            });
-        });
-
-        abp.event.on('passwordChanged', updateConcurrencyStamp);
-
-        const modal = $('UpdateAvatarModal');
-        const src = document.getElementById('crr-user-avt').getAttribute('vnc-src');
-
-        modal.on('shown.bs.modal', () => {
-            console.log(src);
-        }).on('hidden.bs.modal', () => {
-            modal.clearForm();
-        });
-
-        function updateConcurrencyStamp() {
-            _profileService.get().then(function (profile) {
-                $("#ConcurrencyStamp").val(profile.concurrencyStamp);
-            });
+        if (!personalInfoForm.valid()) {
+            return false;
         }
+
+        const input = personalInfoForm.serializeFormToObject();
+
+        _profileService.update(input).done(function () {
+            abp.notify.info(l(LKConstants.SavedSuccessfully));
+            updateConcurrencyStamp();
+        });
     });
-})(jQuery);
+
+    changePasswordForm.submit(function (e) {
+        e.preventDefault();
+
+        if (!changePasswordForm.valid()) {
+            return false;
+        }
+
+        const input = changePasswordForm.serializeFormToObject();
+
+        if (input.newPassword !== input.newPasswordConfirm || input.newPassword.length === 0) {
+            abp.message.error(l(LKConstants.NewPasswordConfirmFailed));
+            return;
+        }
+
+        if (!input || input.currentPassword.length === 0) {
+            return;
+        }
+
+        _profileService.changePassword(input).then(function () {
+            abp.message.success(l(LKConstants.PasswordChanged));
+            abp.event.trigger('passwordChanged');
+            changePasswordForm.trigger("reset");
+        });
+    });
+
+    abp.event.on('passwordChanged', updateConcurrencyStamp);
+
+    const modal = $('UpdateAvatarModal');
+    const src = document.getElementById('crr-user-avt').getAttribute('vnc-src');
+
+    modal.on('shown.bs.modal', () => {
+        console.log(src);
+    }).on('hidden.bs.modal', () => {
+        modal.clearForm();
+    });
+
+    function updateConcurrencyStamp() {
+        _profileService.get().then(function (profile) {
+            $("#ConcurrencyStamp").val(profile.concurrencyStamp);
+        });
+    }
+});
