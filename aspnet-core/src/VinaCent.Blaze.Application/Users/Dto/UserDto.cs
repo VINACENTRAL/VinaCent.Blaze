46262df24284198b46fed612217811a6692c1972
rogzi.tiger@gmail.com <Rogzi>
2022-08-11 21:43:10 +0700

    Update user manage UI

--- aspnet-core/src/VinaCent.Blaze.Application/Users/Dto/UserDto.cs
+++ aspnet-core/src/VinaCent.Blaze.Application/Users/Dto/UserDto.cs
@@ -34,7 +34,45 @@ namespace VinaCent.Blaze.Users.Dto
         public DateTime? LastLoginTime { get; set; }
 
         public DateTime CreationTime { get; set; }
+        
+        public bool IsEmailConfirmed { get; set; }
+        
+        public bool IsPhoneNumberConfirmed { get; set; }
+        
+        public bool IsTwoFactorEnabled { get; set; }
 
         public string[] RoleNames { get; set; }
+
+        #region Profile
+
+        [StringLength(50)]
+        public string Country { get; set; }
+
+        [StringLength(50)]
+        public string State { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [StringLength(10)]
+        public string ZipCode { get; set; }
+
+        public string Description { get; set; }
+
+        [StringLength(255)]
+        public string Avatar { get; set; }
+
+        [StringLength(255)]
+        public string Background { get; set; }
+
+        public DateTime? Birthday { get; set; }
+
+        [StringLength(14)]
+        public string IdentityCardNumber { get; set; }
+
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+
+        #endregion
     }
 }
--- aspnet-core/src/VinaCent.Blaze.Web.Mvc/wwwroot/view-resources/Views/Users/Index.js
+++ aspnet-core/src/VinaCent.Blaze.Web.Mvc/wwwroot/view-resources/Views/Users/Index.js
@@ -1,83 +1,39 @@
 ﻿(function ($) {
-    var _userService = abp.services.app.user,
-        l = abp.localization.getSource('Blaze'),
-        _$modal = $('#UserCreateModal'),
-        _$form = _$modal.find('form'),
-        _$table = $('#UsersTable');
+    var _userService = abp.services.app.user, l = abp.localization.getSource('Blaze'), _$modal = $('#UserCreateModal'),
+        _$form = _$modal.find('form'), _$table = $('#UsersTable');
+
+    $('#UsersSearchForm').submit((e) => {
+        e.preventDefault();
+    });
 
     var _$usersTable = _$table.DataTable({
-        paging: true,
-        serverSide: true,
-        listAction: {
-            ajaxFunction: _userService.getAll,
-            inputFilter: function () {
+        paging: true, serverSide: true, listAction: {
+            ajaxFunction: _userService.getAll, inputFilter: function () {
                 return $('#UsersSearchForm').serializeFormToObject(true);
             }
-        },
-        buttons: [
-            {
-                name: 'refresh',
-                text: '<i class="fas fa-redo-alt"></i>',
-                className: 'waves-effect waves-light',
-                action: () => _$usersTable.draw(false)
-            }
-        ],
-        responsive: {
+        }, buttons: [{
+            name: 'refresh',
+            text: '<i class="fas fa-redo-alt"></i>',
+            className: 'waves-effect waves-light',
+            action: () => _$usersTable.draw(false)
+        }], responsive: {
             details: {
                 type: 'column'
             }
-        },
-        columnDefs: [
-            {
-                targets: 0,
-                className: 'control',
-                defaultContent: '',
-            },
-            {
-                targets: 1,
-                data: 'userName',
-                sortable: false
-            },
-            {
-                targets: 2,
-                data: 'fullName',
-                sortable: false
-            },
-            {
-                targets: 3,
-                data: 'emailAddress',
-                sortable: false
-            },
-            {
-                targets: 4,
-                data: 'isActive',
-                sortable: false,
-                render: data => `<input type="checkbox" disabled ${data ? 'checked' : ''}>`
-            },
-            {
-                targets: 5,
-                data: null,
-                sortable: false,
-                autoWidth: false,
-                defaultContent: '',
-                render: (data, type, row, meta) => {
-                    return [
-                        `   <button type="button" class="btn btn-sm btn-primary edit-user" data-user-id="${row.id}" data-bs-toggle="modal" data-bs-target="#UserEditModal">`,
-                        `       <i class="fas fa-pencil-alt"></i> ${l(LKConstants.Edit)}`,
-                        '   </button>',
-                        `   <button type="button" class="btn btn-sm btn-danger delete-user" data-user-id="${row.id}" data-user-name="${row.name}">`,
-                        `       <i class="fas fa-trash"></i> ${l(LKConstants.Delete)}`,
-                        '   </button>'
-                    ].join('');
-                }
-            }
-        ]
+        }, columnDefs: [{
+            targets: 0,
+            data: 'userName',
+            sortable: false,
+            render: (data, type, row) => buildUserBox(row),
+            className: 'p-0 border-bottom-0'
+        }], createdRow: function (row, data, dataIndex) {
+            $(row).attr('class', 'col');
+        }
     });
 
     _$form.validate({
         rules: {
-            Password: "required",
-            ConfirmPassword: {
+            Password: "required", ConfirmPassword: {
                 equalTo: "#Password"
             }
         }
@@ -119,22 +75,16 @@
     });
 
     function deleteUser(userId, userName) {
-        abp.message.confirm(
-            abp.utils.formatString(
-                l(LKConstants.AreYouSureWantToDelete),
-                userName),
-            null,
-            (isConfirmed) => {
-                if (isConfirmed) {
-                    _userService.delete({
-                        id: userId
-                    }).done(() => {
-                        abp.notify.info(l(LKConstants.SuccessfullyDeleted));
-                        _$usersTable.ajax.reload();
-                    });
-                }
+        abp.message.confirm(abp.utils.formatString(l(LKConstants.AreYouSureWantToDelete), userName), null, (isConfirmed) => {
+            if (isConfirmed) {
+                _userService.delete({
+                    id: userId
+                }).done(() => {
+                    abp.notify.info(l(LKConstants.SuccessfullyDeleted));
+                    _$usersTable.ajax.reload();
+                });
             }
-        );
+        });
     }
 
     $(document).on('click', '.edit-user', function (e) {
@@ -171,10 +121,72 @@
         _$usersTable.ajax.reload();
     });
 
-    $('.txt-search').on('keypress', (e) => {
-        if (e.which == 13) {
-            _$usersTable.ajax.reload();
-            return false;
-        }
+    $('.txt-search').on('input', () => {
+        _$usersTable.ajax.reload();
     });
+
+    function buildUserBox(row) {
+        return `
+        <div class="card team-box ${row.isTwoFactorEnabled ? 'ribbon-box border overflow-hidden' : ''}">
+            <div class="team-cover">
+                <img src="${row.background ?? row.avatar}" alt="${row.fullName}" class="img-fluid" onerror="this.src='${abp.setting.get('App.SiteHolderImage')}'"/>
+            </div>
+            <div class="card-body p-4">
+                ${row.isTwoFactorEnabled ? `
+                <div class="ribbon ribbon-info ribbon-shape trending-ribbon">
+                    <span class="trending-ribbon-text">2FA OK</span> <i class="ri-shield-user-line text-white align-bottom float-end ms-1"></i>
+                </div>
+                ` : ''}
+                <div class="row align-items-center team-row">
+                    <div class="col team-settings">
+                        <div class=" text-end dropdown">
+                            <a href="javascript:void(0);" id="dropdownMenuLink2" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="ri-more-fill fs-17"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink2">
+                                <li>
+                                    <button type="button" class="dropdown-item edit-user" data-user-id="${row.id}" data-bs-toggle="modal" data-bs-target="#UserEditModal"><i class="fas fa-pencil-alt me-2 align-middle"></i> ${l(LKConstants.Edit)}</button>
+                                </li>
+                                <li>
+                                    <button class="dropdown-item delete-user" data-user-id="${row.id}" data-user-name="${row.name}"> <i class="ri-delete-bin-5-line me-2 align-middle"> </i>${l(LKConstants.Delete)} </button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col">
+                        <div class="team-profile-img">
+                            <div class="avatar-lg img-thumbnail rounded-circle shadow flex-shrink-0">
+                                <img src="${row.avatar}" alt="${row.fullName}" class="img-fluid d-block rounded-circle" onerror="this.src='${abp.setting.get('App.SiteUserAvatarHolder')}'"/>
+                            </div>
+                            <div class="team-content">
+                                <a data-bs-toggle="offcanvas" href="#offcanvasExample" aria-controls="offcanvasExample">
+                                    <h5 class="fs-16 mb-1 ${row.isActive ? '' : 'text-decoration-line-through'}">${row.fullName}${(row.isEmailConfirmed || row.isPhoneNumberConfirmed) ? '<i class="ri-checkbox-circle-fill align-middle text-info ms-1"></i>' : ''}</h5>
+                                </a>
+                                <p class="text-muted mb-0">${row.roleNames.join(', ')}</p>
+                            </div>
+                        </div>
+                    </div>
+<!--                    <div class="col-lg-4 col">-->
+<!--                        <div class="row text-muted text-center">-->
+<!--                            <div class="col-6 border-end border-end-dashed">-->
+<!--                                <h5 class="mb-1">225</h5>-->
+<!--                                <p class="text-muted mb-0">Projects</p>-->
+<!--                            </div>-->
+<!--                            <div class="col-6">-->
+<!--                                <h5 class="mb-1">197</h5>-->
+<!--                                <p class="text-muted mb-0">Tasks</p>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+                    <div class="col-lg-2 col">
+                        <div class="text-end">
+                            <a href="pages-profile.html" class="btn btn-light view-btn"> View Profile </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+
 })(jQuery);
