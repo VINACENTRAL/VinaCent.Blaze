3ed3380d9475f091d61b69ee4e9de3f95e0a3623
Minh <baominh9821@gmail.com>
2022-08-19 09:14:07 +0700

    add User  ListSocialNetwork

---
+++ aspnet-core/src/VinaCent.Blaze.Application/Profiles/Dto/SocialNetwork.cs
@@ -0,0 +1,18 @@
+﻿namespace VinaCent.Blaze.Profiles.Dto
+{
+    /// <summary>
+    /// Liên kết mạng xã hội
+    /// </summary>
+    public class SocialNetwork
+    {
+        /// <summary>
+        /// Icon
+        /// </summary>
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// Đường dẫn liên kết
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
